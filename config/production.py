e88d94500635e8886d5f6bfdb63e3b4140b537b3
G1Martoz <g1martinozuna@gmail.com>
2025-03-12 20:17:57 -0300

    Actualizado nuevo settings y un produccion.py para manejar produccion de manera eficiente

---
+++ config/production.py
@@ -0,0 +1,52 @@
+from .settings import *
+
+DEBUG = False
+ALLOWED_HOSTS = ['martoz.pythonanywhere.com']
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
+
+# Static y Media files
+STATIC_ROOT = '/home/Martoz/NewPortfolio/staticfiles'
+MEDIA_ROOT = '/home/Martoz/NewPortfolio/media'
+
+# Mantener whitenoise en producción
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+# Logging para producción
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': '/home/Martoz/NewPortfolio/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
--- config/settings.py
+++ config/settings.py
@@ -11,21 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wf30xw&*qzw@1fx*naisfo9pv3^#0^d$i6a0zd(o)7^4ste)h!'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['Martoz.pythonanywhere.com']
-
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -36,30 +22,27 @@ INSTALLED_APPS = [
     # Local apps
     'portfolio.apps.PortfolioConfig',
     'blog.apps.BlogConfig',
-    'django_ckeditor_5',  # Cambiado de 'ckeditor' a 'django_ckeditor_5'
+    'django_ckeditor_5',
 ]
 
 # CKEditor 5 Configuration
-# CKEditor 5 settings
 CKEDITOR_5_CONFIGS = {
     'default': {
         'toolbar': ['heading', '|', 'bold', 'italic', 'link',
                    'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', ],
         'height': '400px',
-        'skin': 'dark',  # Añadido tema oscuro
+        'skin': 'dark',
         'style': {
-            'content': 'body { color: black; background: white; }'
+            'content': 'body { color: white; background: transparent; }'
         }
     }
 }
 
-# Configuración para subida de archivos de CKEditor 5
 CKEDITOR_5_FILE_STORAGE = "django.core.files.storage.FileSystemStorage"
 CKEDITOR_5_UPLOAD_PATH = "uploads/"
-
-# Asegúrate de que esta configuración esté después de STATIC_URL
 CKEDITOR_5_CUSTOM_CSS = 'blog/admin/css/ckeditor-custom.css'
 
+# Base Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -75,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],  # Add this line
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -90,25 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
-}
-
-
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -124,53 +89,30 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
 LANGUAGE_CODE = 'es-ar'
-
-TIME_ZONE = 'America/Argentina/Buenos_Aires'  # Zona horaria de Buenos Aires
-
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 USE_I18N = True
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    BASE_DIR / "portfolio" / "static", # statics from Portfolio app
-    BASE_DIR / "blog" / "static",  # statics from Blog app
+    BASE_DIR / "portfolio" / "static",
+    BASE_DIR / "blog" / "static",
 ]
 
-# Media files
+# Media files base configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/home/Martoz/NewPortfolio/media'
-
-# Agregar Whitenoise para archivos estáticos
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # Agregar esta línea
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-# Local settings
+# Import local settings if they exist
 try:
     from .local_settings import *
 except ImportError:
-    pass
+    try:
+        from .production import *
+    except ImportError:
+        pass
